0c52e4b50412492e0091b85f04cb6bb36fa09153
Fredrik Udengen <fredrikudengen@gmail.com>
2026-04-04 20:38:25 +0200

    added more useful checkpoints

--- main.py
+++ main.py
@@ -1,46 +1,49 @@
 from fastapi import FastAPI
-from prices import cheapest, get_average_from_db, fetch_prices_from_db, cheapest_from_db
+from prices import cheapest, get_average_from_db, fetch_prices_from_db, cheapest_from_db, fetch_and_save_timeframe, \
+    fetch_and_save_day, cheapest_timeframe
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from prices import fetch_prices, save_prices
-from datetime import date as DateType
+from datetime import date as DateType, timedelta
+
 app = FastAPI()
 
 scheduler = AsyncIOScheduler()
 
 @app.get("/admin/fetch-date")
-async def manual_fetch(date: DateType):
-    await fetch_and_save(date)
+async def manual_fetch(from_date: DateType, to_date: DateType):
+    await fetch_and_save_timeframe(from_date, to_date)
     return {"status": "done"}
 
 @app.on_event("startup")
 async def start_scheduler():
-    scheduler.add_job(fetch_and_save, "cron", hour=14, minute=0)
+    scheduler.add_job(fetch_and_save_day, "cron", hour=14, minute=0)
     scheduler.start()
 
-async def fetch_and_save(date=None):
-    if date is None:
-        date = DateType.today()
-    for region in ["NO1", "NO2", "NO3", "NO4", "NO5"]:
-        daily = await fetch_prices(region, date)
-        save_prices(daily)
-
-@app.get("/prices/today")
-async def get_today_prices(region: str = "NO5"):
-    prices = fetch_prices_from_db(region)
+@app.get("/prices/day")
+async def get_today_prices(day: DateType=DateType.today(), region: str = "NO5"):
+    prices = fetch_prices_from_db(day, region)
     if not prices:
-        return await fetch_prices(region, DateType.today())
+        return await fetch_prices(day, region)
     else:
         return prices
 
-@app.get("/prices/cheapest")
-async def get_cheapest_prices(region: str = "NO5"):
-    cheapest_price = cheapest_from_db(region)
+@app.get("/prices/cheapest-day")
+async def get_cheapest_price_day(day: DateType=DateType.today(), region: str = "NO5"):
+    cheapest_price = cheapest_from_db(day, region)
     if not cheapest_price:
-        daily = await fetch_prices(region, DateType.today())
+        daily = await fetch_prices(day, region)
         return cheapest(daily)
     else:
         return cheapest_price
 
+@app.get("/prices/cheapest-timeframe")
+async def get_cheapest_price_timeframe(from_date: DateType, to_date: DateType, region: str = "NO5"):
+    if from_date == DateType.today():
+        await fetch_and_save_day(from_date)
+    elif to_date == DateType.today():
+        await fetch_and_save_day(to_date)
+    return cheapest_timeframe(from_date, to_date, region)
+
 @app.get("/prices/average")
-async def get_average_prices(region: str = "NO5", days: int = 7):
-    return get_average_from_db(region, days)
+async def get_average_prices(day: DateType = DateType.today(), region: str = "NO5", days: int = 7):
+    return get_average_from_db(day, region, days)
--- prices.py
+++ prices.py
@@ -1,6 +1,8 @@
 import httpx
 from datetime import datetime
 
+from fastapi import HTTPException
+
 from database import SessionLocal
 from db_models import PowerPrice
 from models import HourlyPrice, DailyPrices
@@ -9,8 +11,8 @@ from datetime import date as DateType
 from sqlalchemy.dialects.postgresql import insert
 from sqlalchemy import func
 
-async def fetch_prices(region: str, date: DateType) -> DailyPrices:
-    url = f"https://www.hvakosterstrommen.no/api/v1/prices/{date.strftime("%Y/%m-%d")}_{region}.json"
+async def fetch_prices(day: DateType, region: str) -> DailyPrices:
+    url = f"https://www.hvakosterstrommen.no/api/v1/prices/{day.strftime("%Y/%m-%d")}_{region}.json"
 
     async with httpx.AsyncClient() as client:
         response = await client.get(url)
@@ -24,15 +26,38 @@ async def fetch_prices(region: str, date: DateType) -> DailyPrices:
 
     return DailyPrices(
         region=region,
-        date=date,
+        date=day,
         prices=prices
     )
 
-def fetch_prices_from_db(region: str) -> DailyPrices:
+async def fetch_and_save_timeframe(from_date: DateType, to_date: DateType):
+    start_date = from_date
+    for region in ["NO1", "NO2", "NO3", "NO4", "NO5"]:
+        if from_date <= to_date:
+            while from_date <= to_date:
+                daily = await fetch_prices(from_date, region)
+                save_prices(daily)
+                from_date = from_date + timedelta(days=1)
+            from_date = start_date
+        else:
+            while from_date >= to_date:
+                daily = await fetch_prices(from_date, region)
+                save_prices(daily)
+                from_date = from_date - timedelta(days=1)
+            from_date = start_date
+
+async def fetch_and_save_day(day=None):
+    if day is None:
+        day = DateType.today()
+    for region in ["NO1", "NO2", "NO3", "NO4", "NO5"]:
+        daily = await fetch_prices(day, region)
+        save_prices(daily)
+
+def fetch_prices_from_db(day: DateType, region: str) -> DailyPrices:
     db = SessionLocal()
     result = db.query(PowerPrice)\
             .filter(PowerPrice.region == region)\
-            .filter(PowerPrice.date == DateType.today())\
+            .filter(PowerPrice.date == day)\
             .all()
     db.close()
 
@@ -48,11 +73,38 @@ def cheapest(daily_prices: DailyPrices):
     prices = daily_prices.prices
     return min(prices, key=lambda x: x.price_nok)
 
-def cheapest_from_db(region: str):
+def cheapest_timeframe(from_date: DateType, to_date: DateType, region: str):
+    cheapest_so_far = 10000000000.0
+    cheapest_day = None
+    if from_date <= to_date:
+        while from_date <= to_date:
+            new_cheapest = cheapest_from_db(from_date, region)
+            if new_cheapest is None:
+                from_date = from_date + timedelta(days=1)
+                continue
+            if cheapest_so_far > new_cheapest.price_nok:
+                cheapest_so_far = new_cheapest.price_nok
+                cheapest_day = from_date
+            from_date = from_date + timedelta(days=1)
+    else:
+        while from_date >= to_date:
+            new_cheapest = cheapest_from_db(from_date, region)
+            if new_cheapest is None:
+                from_date = from_date - timedelta(days=1)
+                continue
+            if cheapest_so_far > new_cheapest.price_nok:
+                cheapest_so_far = new_cheapest.price_nok
+                cheapest_day = from_date
+            from_date = from_date - timedelta(days=1)
+    if cheapest_day is None:
+        raise HTTPException(status_code=404, detail="Ingen priser funnet for denne perioden")
+    return fetch_prices_from_db(cheapest_day, region)
+
+def cheapest_from_db(day: DateType, region: str):
     db = SessionLocal()
     result = db.query(PowerPrice)\
             .filter(PowerPrice.region == region)\
-            .filter(PowerPrice.date == DateType.today())\
+            .filter(PowerPrice.date == day)\
             .order_by(PowerPrice.price)\
             .first()
     db.close()
@@ -60,10 +112,9 @@ def cheapest_from_db(region: str):
         return HourlyPrice(hour=result.hour, price_nok=result.price)
     return None
 
-
-def get_average_from_db(region: str, days: int) -> float:
+def get_average_from_db(day: DateType, region: str, days: int) -> float:
     db = SessionLocal()
-    cutoff = DateType.today() - timedelta(days=days)
+    cutoff = day - timedelta(days=days)
     result = db.query(func.avg(PowerPrice.price))\
                .filter(PowerPrice.region == region)\
                .filter(PowerPrice.date >= cutoff)\
