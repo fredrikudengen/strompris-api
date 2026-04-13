ce43c257d13cad8e9fb79e652f24c308664073d8
Fredrik Udengen <fredrikudengen@gmail.com>
2026-04-13 18:21:04 +0200

    fixed CORS error

--- api/main.py
+++ api/main.py
@@ -20,7 +20,11 @@ scheduler = AsyncIOScheduler()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://localhost:3000",
+        "https://jubilant-growth-production-cebd.up.railway.app"
+    ],
     allow_methods=["*"],
     allow_headers=["*"],
 )
