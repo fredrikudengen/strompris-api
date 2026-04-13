fcda9845e07607a407ec873623e4dd28be6487ad
Fredrik Udengen <fredrikudengen@gmail.com>
2026-04-13 18:06:32 +0200

    Fix database host i entrypoint.sh

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-DB_HOST=$(echo $DATABASE_URL | sed 's/.*@\(.*\):.*/\1/')
-DB_PORT=$(echo $DATABASE_URL | sed 's/.*:\([0-9]*\)\/.*/\1/')
-
-until pg_isready -h $DB_HOST -p $DB_PORT; do
-    echo "Venter på databasen..."
-    sleep 2
-done
+sleep 5
 alembic upgrade head
 uvicorn main:app --host 0.0.0.0 --port 8000
